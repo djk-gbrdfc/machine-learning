154ca0054c5755a23d786ce97df0e3e8e4a79af2
djk-gbrdfc <56498255+djk-gbrdfc@users.noreply.github.com>
2019-10-13 14:02:20 +0800

    Add files via upload

---
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -0,0 +1,37 @@
+function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
+%GRADIENTDESCENT Performs gradient descent to learn theta
+%   theta = GRADIENTDESCENT(X, y, theta, alpha, num_iters) updates theta by 
+%   taking num_iters gradient steps with learning rate alpha
+
+% Initialize some useful values
+m = length(y); % number of training examples
+J_history = zeros(num_iters, 1);
+
+for iter = 1:num_iters
+
+    % ====================== YOUR CODE HERE ======================
+    % Instructions: Perform a single gradient step on the parameter vector
+    %               theta. 
+    %
+    % Hint: While debugging, it can be useful to print out the values
+    %       of the cost function (computeCost) and gradient here.
+    %
+    
+    theta1 = theta(1);
+    theta2 = theta(2);
+    
+    theta1 = theta1 - 1 / m * alpha * sum(X * theta - y);
+    theta2 = theta2 - 1 / m * alpha * sum(X(:, 2)' * (X * theta - y));
+    
+    theta = [theta1;theta2];
+    %theta = theta - 1 / m * alpha * (X' * (X * theta - y));
+    %上面theta的求解与下面的值和原理相同
+
+    % ============================================================
+
+    % Save the cost J in every iteration    
+    J_history(iter) = computeCost(X, y, theta);
+
+end
+
+end
